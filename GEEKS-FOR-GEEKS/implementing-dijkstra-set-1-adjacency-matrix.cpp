ac6ac836c5273f61ca2d102915b0a6b691e61ec7
MZ Anjum HaQ Heemel <zuhayeerie333@gmail.com>
2024-09-15 11:54:30 +0600

    333

--- GEEKS-FOR-GEEKS/implementing-dijkstra-set-1-adjacency-matrix.cpp
+++ GEEKS-FOR-GEEKS/implementing-dijkstra-set-1-adjacency-matrix.cpp
@@ -1,3 +1,5 @@
+// PRiORiTY QUEUE METHOD !!
+
 class Solution
 {
 public:
@@ -20,6 +22,42 @@ public:
                 {
                     res_dist[adjNode] = DIST + edgeWeight;
                     pq.push({res_dist[adjNode], adjNode});
+                }
+            }
+        }
+        return res_dist;
+    }
+};
+
+
+
+// SET METHOD !!
+
+class Solution
+{
+public:
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        set<pair<int, int>> st;
+        vector<int> res_dist(V, INT_MAX);
+        res_dist[S] = 0;
+        st.insert({0, S});
+        while (st.size())
+        {
+            auto it = *(st.begin());
+            int DIST = it.first;
+            int NODE = it.second;
+            st.erase(it);
+            for (auto it : adj[NODE])
+            {
+                int adjNode = it[0];
+                int edgeWeight = it[1];
+                if (DIST + edgeWeight < res_dist[adjNode])
+                {
+                    if (res_dist[adjNode] != INT_MAX)
+                        st.erase({res_dist[adjNode], adjNode});
+                    res_dist[adjNode] = DIST + edgeWeight;
+                    st.insert({res_dist[adjNode], adjNode});
                 }
             }
         }
